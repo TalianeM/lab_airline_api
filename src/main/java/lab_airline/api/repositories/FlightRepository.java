7351120063fcc8ef21cf7a4ef37ceed4ef8902f7
Taliane Moussassa <talianegloire392@gmail.com>
2023-04-21 09:10:39 +0100

    adds detail to FlightRepository

--- src/main/java/lab_airline/api/repositories/FlightRepository.java
+++ src/main/java/lab_airline/api/repositories/FlightRepository.java
@@ -1,4 +1,12 @@
 package lab_airline.api.repositories;
 
-public interface FlightRepository {
+
+import lab_airline.api.models.Flight;
+import lab_airline.api.models.Passenger;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FlightRepository extends JpaRepository<Flight, Long> {
+
 }
