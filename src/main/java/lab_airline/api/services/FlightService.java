2f7c19d6e3ba218f359c4a016e603ecf33902c60
Taliane Moussassa <talianegloire392@gmail.com>
2023-04-21 09:23:47 +0100

    updates FlightService

--- src/main/java/lab_airline/api/services/FlightService.java
+++ src/main/java/lab_airline/api/services/FlightService.java
@@ -1,5 +1,6 @@
 package lab_airline.api.services;
 
+import jakarta.persistence.Id;
 import lab_airline.api.models.Flight;
 import lab_airline.api.repositories.FlightRepository;
 import org.springframework.stereotype.Service;
@@ -10,11 +11,24 @@ public class FlightService {
     FlightRepository flightRepository;
 
 
-    public Flight addNewFlight(Flight flight){                   // Add details of new Flight
+    public Flight addNewFlight(Flight flight) {                   // Add details of new Flight
         flightRepository.save(flight);
         return flight;
     }
 
-//     
+    public Flight ShowAllFlights(long id) {                 //     Display all available flights
+        flightRepository.findById(id);
+    }
+
+
+    public Flight showSpecificFlight(long id){             //    details of specific flight
+        flightRepository.findOne()
+    }
+
+//    cancel Flight
+
+    public Flight cancelFlight (Flight flight){
+        
+    }
 
 }
